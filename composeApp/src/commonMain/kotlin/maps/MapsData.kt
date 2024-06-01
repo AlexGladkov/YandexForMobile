a0db8534cd08bb06fd34f59fc35e1d5ff25d2727
Denis Zagayevskiy <zagayevskiy@gmail.com>
2024-06-01 21:39:36 +0300

    rebase issues fixes

--- composeApp/src/commonMain/kotlin/maps/MapsData.kt
+++ composeApp/src/commonMain/kotlin/maps/MapsData.kt
@@ -6,10 +6,7 @@ import androidx.compose.ui.graphics.Color
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import worlds.composeapp.generated.resources.Res
-import worlds.composeapp.generated.resources.browser_contribution_icon
-import worlds.composeapp.generated.resources.browser_path_icon
-import worlds.composeapp.generated.resources.browser_team_icon
-import worlds.composeapp.generated.resources.browser_technology_icon
+import worlds.composeapp.generated.resources.browser_summarize_icon
 
 data class MapsItem(
     val title: String,
@@ -20,9 +17,9 @@ data class MapsItem(
 
 fun mapsItems(): List<MapsItem> {
     return listOf(
-        MapsItem("Команда", Color(0xFFFDFFE5), Res.drawable.browser_team_icon, 140),
-        MapsItem("Путь развития", Color(0xFFE5F5FF), Res.drawable.browser_path_icon, 120),
-        MapsItem("Вклад в общее дело", Color(0xFFE5F5FF), Res.drawable.browser_contribution_icon, 120),
-        MapsItem("Технологии", Color(0xFFFDFFE5), Res.drawable.browser_technology_icon, 120),
+        MapsItem("Команда", Color(0xFFFDFFE5), Res.drawable.browser_summarize_icon, 140),
+        MapsItem("Путь развития", Color(0xFFE5F5FF), Res.drawable.browser_summarize_icon, 120),
+        MapsItem("Вклад в общее дело", Color(0xFFE5F5FF), Res.drawable.browser_summarize_icon, 120),
+        MapsItem("Технологии", Color(0xFFFDFFE5), Res.drawable.browser_summarize_icon, 120),
     )
 }
